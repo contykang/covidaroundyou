ab1104f1106016b842df774a8254599be9b9b9ed
Conty Kang <contykang@gmail.com>
2021-12-30 21:14:42 -0800

    Adding README.md and remove comment for the csv file download.

--- covid.py
+++ covid.py
@@ -7,13 +7,13 @@ import sys
 
 
 if __name__ == '__main__':
-    #url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
-    #myfile = requests.get(url)
+    url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
+    myfile = requests.get(url)
 
-    #filename = 'rawcovid.csv'
+    filename = 'rawcovid.csv'
     
-    #with open(filename, 'wb') as output_file:
-        #output_file.write(myfile.content)
+    with open(filename, 'wb') as output_file:
+        output_file.write(myfile.content)
 
     with open('rawcovid.csv', newline='') as csvfile:
         datareader = csv.reader(csvfile)
