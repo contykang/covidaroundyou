f308d0eea70a0e91c997af47bdb7e83f326f1e1b
Conty Kang <contykang@gmail.com>
2022-01-07 12:28:11 +1100

    Adding the part for initializing data if first time runnging the script.

--- covid.py
+++ covid.py
@@ -10,8 +10,22 @@ from datetime import datetime
 
 if __name__ == '__main__':
 
-    
     filename = 'rawcovid.csv'
+
+    # Check if the rawcovid.csv file exist (mainly for the first time running the script). Create one empty file if not exist then download the latest data.
+
+    if os.path.isfile('rawcovid.csv') is False:
+        open(filename, 'w+').close()
+        print("Initilizign the data...")
+        url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
+        myfile = requests.get(url)
+
+        with open(filename, 'wb') as output_file:
+            output_file.write(myfile.content)
+
+
+    # Get the last modified date of the file "rawcovid" to see if we need to download a latest version of the file or not.
+
     time = datetime.fromtimestamp(int(os.path.getmtime(filename))).strftime('%Y-%m-%d')
     now = datetime.now().strftime('%Y-%m-%d')
 
@@ -24,23 +38,17 @@ if __name__ == '__main__':
             output_file.write(myfile.content)
     else:
         print("No new data, keep processing...\n")
-        with open('rawcovid.csv', newline='') as csvfile:
-            datareader = csv.reader(csvfile)
+    
+    # Start importing data from csv file and calculate the total case number at each day.
+    with open('rawcovid.csv', newline='') as csvfile:
+        datareader = csv.reader(csvfile)
 
-            thisDate = ''
-            thisCount = 0
+        thisDate = ''
+        thisCount = 0
 
-            for row in datareader:
-                if len(sys.argv) > 1:
-                    if row[1] == sys.argv[1]:
-                        if thisDate != row[0]:
-                            if thisDate != '':
-                                print(thisDate, thisCount)
-                            thisDate = row[0]
-                            thisCount = 1
-                        else:
-                            thisCount += 1
-                else:
+        for row in datareader:
+            if len(sys.argv) > 1:
+                if row[1] == sys.argv[1]:
                     if thisDate != row[0]:
                         if thisDate != '':
                             print(thisDate, thisCount)
@@ -48,4 +56,12 @@ if __name__ == '__main__':
                         thisCount = 1
                     else:
                         thisCount += 1
+            else:
+                if thisDate != row[0]:
+                    if thisDate != '':
+                        print(thisDate, thisCount)
+                    thisDate = row[0]
+                    thisCount = 1
+                else:
+                    thisCount += 1
 
