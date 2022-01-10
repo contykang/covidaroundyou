d4564417ffe7470c661a6c7a761aabe8137e5b56
Conty Kang <contykang@gmail.com>
2022-01-10 11:20:58 +1100

    Adding .gitignore, adding output to csv

--- README.md
+++ README.md
@@ -10,6 +10,8 @@ For example: python3 covid.py 2000
 
 This will show all the reported cases for postcode 2000 (Sydney city) since the pandemic.
 
+If no post code given, all the cases in NSW will be shown.
+
 Source is from NSW Health website:
 
 https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv
--- covid.py
+++ covid.py
@@ -7,21 +7,33 @@ import sys
 import os
 from datetime import datetime
 
+def downloadCsv(filename):
+        url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
+        myfile = requests.get(url)
+
+        with open(filename, 'wb') as output_file:
+            output_file.write(myfile.content)
+
+
+def outputCsv(filename, thisDate, thisCount):
+    # Export the data to a single CSV file for further process
+
+    with open (filename, 'a') as ocsv:
+        addrow = [thisDate, thisCount]
+        write = csv.writer(ocsv)
+        write.writerow(addrow)
+
 
 if __name__ == '__main__':
 
-    filename = 'rawcovid.csv'
+    filename = 'rawcovid.csv'    
 
     # Check if the rawcovid.csv file exist (mainly for the first time running the script). Create one empty file if not exist then download the latest data.
 
     if os.path.isfile('rawcovid.csv') is False:
         open(filename, 'w+').close()
         print("Initilizign the data...")
-        url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
-        myfile = requests.get(url)
-
-        with open(filename, 'wb') as output_file:
-            output_file.write(myfile.content)
+        downloadCsv(filename)
 
 
     # Get the last modified date of the file "rawcovid" to see if we need to download a latest version of the file or not.
@@ -31,11 +43,7 @@ if __name__ == '__main__':
 
     if time != now:
         print("Need to download the new data now...\n")
-        url = 'https://data.nsw.gov.au/data/dataset/aefcde60-3b0c-4bc0-9af1-6fe652944ec2/resource/21304414-1ff1-4243-a5d2-f52778048b29/download/confirmed_cases_table1_location.csv'
-        myfile = requests.get(url)
-
-        with open(filename, 'wb') as output_file:
-            output_file.write(myfile.content)
+        downloadCsv(filename)
     else:
         print("No new data, keep processing...\n")
     
@@ -48,20 +56,27 @@ if __name__ == '__main__':
 
         for row in datareader:
             if len(sys.argv) > 1:
+                output = "covidcount_" + sys.argv[1] + ".csv"
                 if row[1] == sys.argv[1]:
                     if thisDate != row[0]:
                         if thisDate != '':
                             print(thisDate, thisCount)
+                            outputCsv(output, thisDate, thisCount)
                         thisDate = row[0]
                         thisCount = 1
                     else:
                         thisCount += 1
             else:
+                output = "covidcount_NSW.csv"
                 if thisDate != row[0]:
                     if thisDate != '':
                         print(thisDate, thisCount)
+                        outputCsv(output, thisDate, thisCount)
                     thisDate = row[0]
                     thisCount = 1
                 else:
                     thisCount += 1
+
+
+
 
